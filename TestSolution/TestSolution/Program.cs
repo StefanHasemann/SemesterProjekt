38729cdcc01ce42624081f11814f28575c8f0cd9
Stefan Hasemann <1071275@ucn.dk>
2019-02-22 13:12:19 +0100

    LKalalalala

--- TestSolution/TestSolution/Program.cs
+++ TestSolution/TestSolution/Program.cs
@@ -9,6 +9,8 @@ namespace TestSolution
             Console.WriteLine("Hello World!");
             Console.WriteLine("Jacob Was Here");
             Console.WriteLine("Peter Was Here");
+            Console.WriteLine("Stefan Was Here");
+
         }
     }
 }
